63d7db7419af91cd8bd0bee1c470f30fd861fb0d
Ben Bushell <ben.bushell87@icloud.com>
2022-11-17 14:03:04 +0700

    add logged in user profile component

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,1 +1,14 @@
-const Profile = () => {};
+import { useContext } from 'react';
+import { UserContext } from '../contexts/user';
+
+export const Profile = () => {
+  const { loggedInUser } = useContext(UserContext);
+
+  return (
+    <section>
+      <h1>{loggedInUser.username}</h1>
+      <p>{loggedInUser.first_name + ' ' + loggedInUser.surname}</p>
+      <p>{loggedInUser.team}</p>
+    </section>
+  );
+};
