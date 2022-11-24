c9ea362b97b35ff00889339f5844f1d321af5be0
Ben Bushell <ben.bushell87@icloud.com>
2022-11-24 14:06:37 +0700

    use local storage for logged in user context

--- src/contexts/user.js
+++ src/contexts/user.js
@@ -1,10 +1,14 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 export const UserContext = createContext();
 
 export const UserProvider = (props) => {
-  const [loggedInUser, setLoggedInUser] = useState({});
-
+  const [loggedInUser, setLoggedInUser] = useState(
+    JSON.parse(localStorage.loggedInUser)
+  );
+  useEffect(() => {
+    localStorage.setItem('loggedInUser', JSON.stringify(loggedInUser));
+  }, [loggedInUser]);
   return (
     <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
       {props.children}
